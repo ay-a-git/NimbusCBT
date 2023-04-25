ed131f66cd87c51565d0d9ceee750e526f43bef8
ay-a-git <introvert212@aol.com>
2023-04-25 11:01:52 -0400

    Created test timer for messages to return to console

--- nimbusCBT/client/src/App.jsx
+++ nimbusCBT/client/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
 import EmailValidator from "./components/EmailValidation.jsx";
 
-import TimeCapsule from "./components/TimeCapsule.jsx";
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <EmailValidator />
-
       {/* <h1>Email thing</h1>
       <EmailValidator /> */}
       <TimeCapsule />
