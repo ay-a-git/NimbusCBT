1be20af224394011d9a685c78a0da646ded53a51
ay-a-git <introvert212@aol.com>
2023-04-26 13:02:32 -0400

    attempts to add timestamps to timecapsule messages

--- nimbusCBT/client/src/App.jsx
+++ nimbusCBT/client/src/App.jsx
@@ -3,12 +3,9 @@ import EmailValidator from "./components/EmailValidation.jsx";
 import TimeCapsule from "./components/TimeCapsule.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      {/* <h1>Email thing</h1>
-      <EmailValidator /> */}
+      <EmailValidator />
       <TimeCapsule />
     </div>
   );
